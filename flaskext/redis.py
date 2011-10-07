db44d9258866cf605ec50852f7fd401298492ce8
Maxim Bublis <b@codemonkey.ru>
2011-10-07 11:20:19 +0400

    redis.Redis is threadsafe, so there is no need in opening it per request. Interface changed - now there is only init_redis function instead of Redis class.

--- flaskext/redis.py
+++ flaskext/redis.py
@@ -1,37 +1,18 @@
-"""
-"""
+"""Flask integration for Redis"""
 
 from __future__ import absolute_import
 import redis
-from flask import g
 
-class Redis(object):
 
-    def __init__(self, app=None):
+def init_redis(app):
+    """Initializes Redis client from app config"""
 
-        if app is not None:
-            self.init_app(app)
-        else:
-            self.app = None
+    app.config.setdefault('REDIS_HOST', 'localhost')
+    app.config.setdefault('REDIS_PORT', 6379)
+    app.config.setdefault('REDIS_DB', 0)
+    app.config.setdefault('REDIS_PASSWORD', None)
 
-    def init_app(self, app):
-        """
-        Used to initialize redis with app object
-        """
-        app.config.setdefault('REDIS_HOST', 'localhost')
-        app.config.setdefault('REDIS_PORT', 6379)
-        app.config.setdefault('REDIS_DB', 0)
-        app.config.setdefault('REDIS_PASSWORD', None)
-
-        app.before_request(self.before_request)
-
-        self.app = app
-
-    def connect(self):
-        return redis.Redis(host=self.app.config['REDIS_HOST'],
-                           port=self.app.config['REDIS_PORT'],
-                           db=self.app.config['REDIS_DB'],
-                           password=self.app.config['REDIS_PASSWORD'])
-
-    def before_request(self):
-        g.redis = self.connect()
+    return redis.Redis(host=app.config['REDIS_HOST'],
+                       port=app.config['REDIS_PORT'],
+                       db=app.config['REDIS_DB'],
+                       password=app.config['REDIS_PASSWORD'])
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ redis-py library.
 """
 from setuptools import setup
 
-version = '0.1'
+version = '0.2'
 
 setup(
     name='Flask-Redis',
