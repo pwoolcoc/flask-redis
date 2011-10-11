ca1702b8df79c5b4328b3179fba8c9c997a64b71
Paul Woolcock <pwoolcoc+github@gmail.com>
2011-10-11 17:58:39 -0300

    made redis ext. more conforming to flaskext standards. See [Flask Extension Development -- Flask 0.8 documentation](http://flask.pocoo.org/docs/extensiondev/#initializing-extensions)

--- flaskext/redis.py
+++ flaskext/redis.py
@@ -1,18 +1,23 @@
 """Flask integration for Redis"""
 
-from __future__ import absolute_import
 import redis
 
+class Redis(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.app = app
+            self.init_app(app)
+        else:
+            self.app = None
+    
+    def init_app(self, app):
+        self.app = app
+        self.app.config.setdefault(u"REDIS_HOST", u"localhost")
+        self.app.config.setdefault(u"REDIS_POST", 6379)
+        self.app.config.setdefault(u"REDIS_DB", 0)
+        self.app.config.setdefault(u"REDIS_PASSWORD", None)
+        self.redis_instance = redis.Redis(host=self.app.config[u"REDIS_HOST"],
+                                          port=self.app.config[u"REDIS_PORT"],
+                                          db=self.app.config[u"REDIS_DB"],
+                                          password=self.app.config[u"REDIS_PASSWORD"])
 
-def init_redis(app):
-    """Initializes Redis client from app config"""
-
-    app.config.setdefault('REDIS_HOST', 'localhost')
-    app.config.setdefault('REDIS_PORT', 6379)
-    app.config.setdefault('REDIS_DB', 0)
-    app.config.setdefault('REDIS_PASSWORD', None)
-
-    return redis.Redis(host=app.config['REDIS_HOST'],
-                       port=app.config['REDIS_PORT'],
-                       db=app.config['REDIS_DB'],
-                       password=app.config['REDIS_PASSWORD'])
