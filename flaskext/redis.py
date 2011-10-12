6af110a3f6895fbba125d2af6e42d0d9e1e5fb4c
Paul Woolcock <pwoolcoc+github@gmail.com>
2011-10-11 21:21:38 -0300

    s/REDIS_POST/REDIS_PORT/

--- flaskext/redis.py
+++ flaskext/redis.py
@@ -13,7 +13,7 @@ class Redis(object):
     def init_app(self, app):
         self.app = app
         self.app.config.setdefault(u"REDIS_HOST", u"localhost")
-        self.app.config.setdefault(u"REDIS_POST", 6379)
+        self.app.config.setdefault(u"REDIS_PORT", 6379)
         self.app.config.setdefault(u"REDIS_DB", 0)
         self.app.config.setdefault(u"REDIS_PASSWORD", None)
         self.redis_instance = redis.Redis(host=self.app.config[u"REDIS_HOST"],
